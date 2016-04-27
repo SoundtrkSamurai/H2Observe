3d6983b80ac1ec0bb1b30a7f33c1bdcdecb02aa4
SoundtrkSamurai <furtimat3@yahoo.com>
2016-04-27 00:23:51 -0700

    Added new plugin so jquery compatitble with Windows Universal App

---
+++ H2Observe/www/app/app.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("h2observe", ['ionic', 'myapp.controllers', 'myapp.services', 'ngCordova', 'ui-leaflet'])
+        .run(function ($ionicPlatform) {
+            $ionicPlatform.ready(function () {
+                if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        })
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+            .state("app", {
+                url: "/app",
+                abstract: true,
+                templateUrl: "app/templates/view-menu.html",
+                controller: "appController as app"
+            })
+            .state('app.map', {
+                url: '/map',
+                templateUrl: 'app/templates/view-map.html',
+                controller: 'mapController as mapCtrl'
+            })
+            .state('app.ble', {
+                url: '/bluetooth',
+                templateUrl: 'app/templates/view-ble.html',
+                controller: 'bleCtrl'
+            })
+            .state("app.home", {
+                url: "/home",
+                templateUrl: "app/templates/view-home.html",
+                controller: "homeCtrl"
+            });
+            $urlRouterProvider.otherwise("/app/home");
+        });
+})();
---
+++ H2Observe/www/app/controllers.js
@@ -0,0 +1,112 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("myapp.controllers", [])
+
+    .controller("appController", [function () {
+        var controller = this;
+        controller.companyName = 'H2Observe';
+        controller.mapWidth = $('ion-content').css('width');
+        controller.mapHeight = $('ion-content').css('height');
+    }])
+
+    //homeCtrl provides the logic for the home screen
+    .controller("homeCtrl", ["$scope", "$state", function ($scope, $state) {
+        $scope.refresh = function () {
+            //refresh binding
+            $scope.$broadcast("scroll.refreshComplete");
+        };
+    }])
+
+    .controller('mapController', ['$http', '$log', '$state', '$ionicLoading', '$cordovaGeolocation', 'leafletData',
+        function ($http, $log, $state, $ionicLoading, $cordovaGeolocation, leafletData) {
+
+        $log.log('-> mapController');
+        var controller = this;
+
+        // Set center of map on first run through
+        if (!controller.center) {
+            angular.extend(controller, {
+                center: {
+                    lat: 47.6487,
+                    lng: -122.1168,
+                    zoom: 10
+                }
+            });
+        }
+
+        leafletData.getMap('mymap')
+            .then(function (map) {
+                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                    maxZoom: 20,
+                    id: 'soundtrksamurai.pm2fphn7',
+                    accessToken: 'pk.eyJ1Ijoic291bmR0cmtzYW11cmFpIiwiYSI6ImNpbXlnb2JrZjA0MjJ1d200eTB2YjlnOG8ifQ.VQrBuf4kLiRrHj1v884qJw'
+                }).addTo(map);
+                var lc = L.control.locate({ drawCircle: false, locateOptions: { enableHighAccuracy: true } }).addTo(map);
+                map.removeControl(map.zoomControl);
+                lc.start();
+            });
+
+        //controller.GetGeoLocation = function () {
+        //    $log.log('Tracing current location...');
+        //    $ionicLoading.show({
+        //        template: 'Tracing current location...'
+        //    });
+        //    $cordovaGeolocation.getCurrentPosition()
+        //        .then(function (position) {
+        //            $log.log('Current location found');
+        //            $log.log('Current location Latitude' + position.coords.latitude);
+        //            $log.log('Current location Longitude' + position.coords.longitude);
+        //            $ionicLoading.hide();
+        //            angular.extend(controller, {
+        //                center: {
+        //                    lat: position.coords.latitude,
+        //                    lng: position.coords.longitude,
+        //                    zoom: 8
+        //                },
+        //                markers: {
+        //                    marker: {
+        //                        lat: position.coords.latitude,
+        //                        lng: position.coords.longitude,
+        //                        draggable: false
+        //                    }
+        //                }
+        //            });
+        //            controller.latLang.lat = parseFloat(position.coords.latitude);
+        //            controller.latLang.lang = parseFloat(position.coords.longitude);
+        //            var lat = controller.latLang.lat;
+        //        });
+        //};
+    }])
+
+    .controller('bleCtrl', ['$scope', function ($scope) {
+
+    }])
+
+    //errorCtrl managed the display of error messages bubbled up from other controllers, directives, myappService
+    .controller("errorCtrl", ["$scope", "myappService", function ($scope, myappService) {
+        //public properties that define the error message and if an error is present
+        $scope.error = "";
+        $scope.activeError = false;
+
+        //function to dismiss an active error
+        $scope.dismissError = function () {
+            $scope.activeError = false;
+        };
+
+        //broadcast event to catch an error and display it in the error section
+        $scope.$on("error", function (evt, val) {
+            //set the error message and mark activeError to true
+            $scope.error = val;
+            $scope.activeError = true;
+
+            //stop any waiting indicators (including scroll refreshes)
+            myappService.wait(false);
+            $scope.$broadcast("scroll.refreshComplete");
+
+            //manually apply given the way this might bubble up async
+            $scope.$apply();
+        });
+    }]);
+})();
